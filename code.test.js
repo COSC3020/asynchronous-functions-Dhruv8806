1f9fe552f30f45963070a123d25d2f11c6f49da0
Dhruv8806 <123929419+Dhruv8806@users.noreply.github.com>
2023-12-01 13:35:20 -0700

    Create code.test.js

---
+++ code.test.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const jsc = require('jsverify');
+
+// const async = require('async');
+eval(fs.readFileSync('code.js')+'');
+
+const countMatchesProperty = jsc.forall(jsc.array(jsc.number), jsc.number, async function (array, key) {
+  const expectedCount = array.filter(element => element === key).length;
+
+  const result = await new Promise((resolve) => code.countAsync(array, key, resolve));
+  return result === expectedCount;
+});
+
